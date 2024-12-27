8a5d3375ca6ed5be91e67e760ce7e1ba85ae184b
Luan Ujaque <120577867+luanujk@users.noreply.github.com>
2024-12-27 15:25:52 -0300

    Add files via upload

---
+++ Exercicio10.js
@@ -0,0 +1,8 @@
+function simboloMais (quantidadeDeSinais) {
+  let resultado = '';
+  for (let i = 0; i < quantidadeDeSinais; i++) {
+    resultado += '+';
+  } console.log(resultado);
+}
+
+simboloMais(10)
